9fbe0bfe27c516eef9cbae0151c33d0bd2149cc4
kstensing <stensingk@gmail.com>
2022-02-13 22:47:05 -0600

    refactored and added promises

---
+++ utils/generate-site.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true, 
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true, 
+                message: 'Copy successful!'
+            });
+        });
+    });
+};
+
+module.exports = { writeFile, copyFile};
